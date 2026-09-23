524bea15380546249e5e501a0bbe3950d97ae6a3
Anushree Bhargava <anushreebhargavacollege@gmail.com>
2026-09-23 21:28:41 +0530

    Added Module 1 and Module 2 projects

---
+++ first-app/src/main/java/com/example/myapp/App.java
@@ -0,0 +1,13 @@
+package com.example.myapp;
+
+/**
+ * Simple Java application.
+ */
+public class App
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello from My Java Application!");
+        System.out.println("This project has been modified for my practical work.");
+    }
+}
